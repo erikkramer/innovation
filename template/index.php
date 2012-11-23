d7e32c22b2418bbf87896af0356a62a02f207b6b
Erik Kramer <ekramer@erikkramer.eu>
2012-11-23 16:56:50 +0100

    bla

--- template/index.php
+++ template/index.php
@@ -1,122 +1,28 @@
+<!DOCTYPE html>
+<!-- 
+Copyright Studio HIER 2011    
+-->
+<html>
 
-<!doctype html>
-<!--[if IEMobile 7]><html
-class="no-js iem7"><![endif]-->
-<!--[if (gt IEMobile 7)|!(IEMobile)]><!--><html
-class="no-js" lang="en"><!--<![endif]-->
-<!--[if lt IE 7]><html
-class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if (IE 7)&!(IEMobile)]><html
-class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
-<!--[if (IE 8)&!(IEMobile)]><html
-class="no-js lt-ie9" lang="en"><![endif]-->
-<!--[if gt IE 8]><!--><html
-class="no-js" lang="en"><!--<![endif]--><head><meta
-charset="utf-8"><title>ReSRC.it - Responsive images on demand, directly from the cloud.</title><meta
-name="description" content="ReSRC.it delivers responsive images on demand, directly from the cloud. Link your images through our super simple API and let us take care of everything."><meta
-name="HandheldFriendly" content="True"><meta
-name="MobileOptimized" content="320"><meta
-name="viewport" content="width=device-width, initial-scale=1.0"><link
-rel="apple-touch-icon" href="/img/resrc_logo_57.png"><link
-rel="apple-touch-icon" sizes="72x72" href="/img/resrc_logo_72.png"><link
-rel="apple-touch-icon" sizes="114x114" href="/img/resrc_logo_114.png">
-<!--[if (lt IE 9) & (!IEMobile)]><script src="/js/selectivizr-min.js"></script><![endif]-->
- <script src="/js/modernizr-2.5.3-min.js"></script> <meta
-name="apple-mobile-web-app-capable" content="yes"><meta
-http-equiv="cleartype" content="on"><link
-rel="stylesheet" href="css/style.css">
-<!--[if lt IE 9]><link
-rel="stylesheet" media="screen" href="/css/ie.css">
-<![endif]--></head><body>
-<header
-class="ss-header fixed">
-<nav>
-<a
-href="/" title="Home" class="logo">ReSRC.it</a><div
-class="menu"><ul><li
-class="current_page_item"><a
-href="http://resrc.it/" title="Home">Home</a></li><li
-class="page_item page-item-7"><a
-href="http://resrc.it/demo/">Demo</a></li><li
-class="page_item page-item-9"><a
-href="http://resrc.it/docs/">Docs</a></li><li
-class="page_item page-item-13"><a
-href="http://resrc.it/signup/">Sign Up</a></li><li
-class="page_item page-item-87"><a
-href="http://resrc.it/contact/">Contact Us</a></li></ul></div>
-</nav>
-</header><div
-class="hero big"><div><h1>
-<span>Responsive images <br/>on demand, direct <br/>from the cloud.</span></h1> <i
-class="img-devices"></i></div></div><div
-class="container">
-<article
-class="clearfix"><div
-class="section group cta"><div
-class="col span_4_of_6"><h4>
-ReSRC.it delivers responsive images on-demand, direct from the cloud. Link your images to our super simple service and let us take care of everything.
-<span
-class="visible-desktop">
-With a hassle free signup you can be up and running in seconds.
-</span></h4></div><div
-class="col span_2_of_6"><div
-class="well"><div
-class="section group"><div
-class="col span_3_of_6"><h4>Sign up<span
-class="hidden-ipad-portrait"> for free</span></h4><p
-class="last-child">We&#8217;re in beta</p></div><div
-class="col span_3_of_6">
-<a
-href="/signup/#signup" class="btn btn-large btn-success right">Signup<span
-class="hidden-ipad-portrait"> Now</span></a></div></div></div></div></div><div
-class="section group"><div
-class="features"><div
-class="section group"><div
-class="page-header"><h2>Features &amp; Benefits</h2></div></div><div
-class="section group"><div
-class="col span_2_of_6"><h4> <i
-class="icon-resize-full icon-large"></i>
-Responsive Images</h4><p>
-Images delivered at the right pixel size for the device viewing your application.</p></div><div
-class="col span_2_of_6"><h4>
-<i
-class="icon-signal icon-large"></i>
-Bandwidth Intelligent</h4><p>
-The right quality of image delivered for your current connection speed.</p></div><div
-class="col span_2_of_6"><h4>
-<i
-class="icon-eye-open icon-large"></i>
-Retina / HiDPI Support</h4><p>
-Full support for the latest screens with enhanced resolution displays.</p></div></div><div
-class="section group"><div
-class="col span_2_of_6"><h4>
-<i
-class="icon-picture icon-large"></i>
-Image Manipulation</h4><p>
-You keep total control of your images, sizes, crops, optimizations and effects.</p></div><div
-class="col span_2_of_6"><h4>
-<i
-class="icon-thumbs-up icon-large"></i>
-No Smoke &amp; Mirrors</h4><p>
-We stay <u>well clear</u> from user agent detection and cookie requests. That's not responsive!</p></div><div
-class="col span_2_of_6"><h4>
-<i
-class="icon-cloud icon-large"></i>
-Cloud Performance</h4><p>
-Your images processed then delivered from our reliable, scalable and supported cloud service.</p></div></div></div></div>
-</article>
-<footer
-id="footer"><div
-class="section group"><div
-class="col span_3_of_6">
-ReSRC.it © 2012 <span
-class="hidden-phone">- Awesomely Responsive.</span></div><div
-class="col span_3_of_6"><ul
-class="right"><li><a
-href="https://twitter.com/ReSRCit" title="Follow us on Twitter" class="btn btn-mini"><i
-class="icon-twitter"></i>Follow<span
-class="hidden-phone"> us on Twitter</span></a></li><li><a
-href="https://facebook.com/ReSRCit" title="Like us on Facebook" class="btn btn-mini"><i
-class="icon-facebook"></i>Like<span
-class="hidden-phone"> us on Facebook</span></a></li></ul></div></div>
-</footer></div> <script>document.write('<script src="/js/'+('__proto__'in{}?'zepto-1.0.min':'jquery-1.7.2.min')+'.js"><\/script>');</script> <script src="/js/js.min.js?0.0.4"></script> <script>(function(){var d={locale:"uk",bandwidthDetect:!0,resrcOnLoad:!0,resrcOnResize:!0,resrcOnPinch:!0,resrcOnDoubleTap:!0},c=!1,a=document.createElement("script");a.src="//use.resrc.it/resrc.js";a.type="text/javascript";a.async="true";a.onload=a.onreadystatechange=function(){var a=this.readyState;if(!(c||a&&"complete"!=a&&"loaded"!=a)){c=!0;try{resrc.load(d)}catch(b){}}};var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})();</script>  <script>var _gaq=[['_setAccount','UA-33945608-1'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'));</script> <!--[if lt IE 7]><script>window.location="http://browsehappy.com/"</script><![endif]--></body></html>
+	<head>
+		<title>Uitgebreidehoroscoop.nl - Onze astrologen stellen de meest persoonlijke en uitgebreide horoscoop voor u samen.</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta charset="utf-8">
+		  
+		<!-- SEO -->
+		<meta name="author" content="http://www.erikpl.us" />
+		<meta content="Uitgebreide persoonlijke horoscoop per email of post" name="description" />
+		<meta content="Horscoop, Sterren, Astrologie" name="keywords" />
+		<META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
+		<!-- Styling -->
+		<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" media="screen" href="/template/css/style.css">
+
+	</head>
+	
+	<body id="">
+
+	<h1>blaaalaaa laal</h1>
+
+	</body>
+</html>
